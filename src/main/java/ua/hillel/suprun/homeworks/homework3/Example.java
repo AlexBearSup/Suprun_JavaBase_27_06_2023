bdd1f4c015b06aa9a97d3c4bacd8cb42c975708f
Suprun Oleksandr <1specktr1@gmail.com>
2023-07-05 10:29:37 +0300

    add new line in file

--- src/main/java/ua/hillel/suprun/homeworks/homework3/Example.java
+++ src/main/java/ua/hillel/suprun/homeworks/homework3/Example.java
@@ -3,7 +3,8 @@ package ua.hillel.suprun.homeworks.homework3;
 public class Example {
     public static void main(String[] args) {
 
-        System.out.println("test git1");
+        System.out.println("test git 1");
+        System.out.println("add new line");
     }
 
 }
