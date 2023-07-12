b04005bb4047416531ee56e8e1166634369dacce
Suprun Oleksandr <1specktr1@gmail.com>
2023-07-12 09:31:57 +0300

    changed query in console from english to ukrainian

--- src/main/java/ua/hillel/suprun/homeworks/homework6/CounterStrike.java
+++ src/main/java/ua/hillel/suprun/homeworks/homework6/CounterStrike.java
@@ -7,116 +7,112 @@ public class CounterStrike {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("enter the name of the first team");
+        System.out.println("введіть назву першої команди");
         String nameTeam1 = scanner.nextLine();
 
-        System.out.println("enter the number of frags Boom ");
+        System.out.println("введіть кількість фрагів гравця Boom ");
         int fragsBoom = 0;
         if (scanner.hasNextInt()) {
             fragsBoom = scanner.nextInt();
         } else {
-            System.out.println("Incorrect value, please start again ");
+            System.out.println("помилкове значення, будь-ласка перезапустіть програму.");
             System.exit(0);
         }
 
-        System.out.println("enter the number of frags Little ");
+        System.out.println("введіть кількість фрагів гравця Little ");
         int fragsLittle = 0;
         if (scanner.hasNextInt()) {
             fragsLittle = scanner.nextInt();
         } else {
-            System.out.println("Incorrect value, please start again ");
+            System.out.println("помилкове значення, будь-ласка перезапустіть програму.");
             System.exit(0);
         }
 
-        System.out.println("enter the number of frags Zeus ");
+        System.out.println("введіть кількість фрагів гравця Zeus ");
         int fragsZeus = 0;
         if (scanner.hasNextInt()) {
             fragsZeus = scanner.nextInt();
         } else {
-            System.out.println("Incorrect value, please start again ");
+            System.out.println("помилкове значення, будь-ласка перезапустіть програму.");
             System.exit(0);
         }
 
-        System.out.println("enter the number of frags MVP ");
+        System.out.println("введіть кількість фрагів гравця MVP ");
         int fragsMVP = 0;
         if (scanner.hasNextInt()) {
             fragsMVP = scanner.nextInt();
         } else {
-            System.out.println("Incorrect value, please start again ");
+            System.out.println("помилкове значення, будь-ласка перезапустіть програму.");
             System.exit(0);
         }
-        System.out.println("enter the number of frags Smile ");
+        System.out.println("введіть кількість фрагів гравця Smile ");
         int fragsSmile = 0;
         if (scanner.hasNextInt()) {
             fragsSmile = scanner.nextInt();
         } else {
-            System.out.println("Incorrect value, please start again ");
+            System.out.println("помилкове значення, будь-ласка перезапустіть програму.");
             System.exit(0);
         }
 
         scanner.nextLine();
-        System.out.println("enter the name of the second team");
+        System.out.println("введіть назву другої команди");
         String nameTeam2 = scanner.nextLine();
 
-        System.out.println("enter the number of frags Dendi ");
+        System.out.println("введіть кількість фрагів гравця Dendi ");
         int fragsDendi = 0;
         if (scanner.hasNextInt()) {
             fragsDendi = scanner.nextInt();
         } else {
-            System.out.println("Incorrect value, please start again ");
+            System.out.println("помилкове значення, будь-ласка перезапустіть програму.");
             System.exit(0);
         }
 
-        System.out.println("enter the number of frags XBOCT ");
+        System.out.println("введіть кількість фрагів гравця XBOCT ");
         int fragsXBOCT = 0;
         if (scanner.hasNextInt()) {
             fragsXBOCT = scanner.nextInt();
         } else {
-            System.out.println("Incorrect value, please start again ");
+            System.out.println("помилкове значення, будь-ласка перезапустіть програму.");
             System.exit(0);
         }
 
-        System.out.println("enter the number of frags Puppey ");
+        System.out.println("введіть кількість фрагів гравця Puppey ");
         int fragsPuppey = 0;
         if (scanner.hasNextInt()) {
             fragsPuppey = scanner.nextInt();
         } else {
-            System.out.println("Incorrect value, please start again ");
+            System.out.println("помилкове значення, будь-ласка перезапустіть програму.");
             System.exit(0);
         }
 
-        System.out.println("enter the number of frags Solo ");
+        System.out.println("введіть кількість фрагів гравця Solo ");
         int fragsSolo = 0;
         if (scanner.hasNextInt()) {
             fragsSolo = scanner.nextInt();
         } else {
-            System.out.println("Incorrect value, please start again ");
+            System.out.println("помилкове значення, будь-ласка перезапустіть програму.");
             System.exit(0);
         }
-        System.out.println("enter the number of frags Dread ");
+        System.out.println("введіть кількість фрагів гравця Dread ");
         int fragsDread = 0;
         if (scanner.hasNextInt()) {
             fragsDread = scanner.nextInt();
         } else {
-            System.out.println("Incorrect value, please start again ");
+            System.out.println("помилкове значення, будь-ласка перезапустіть програму.");
             System.exit(0);
         }
 
-        double pointsTeam1 = (fragsBoom + fragsLittle + fragsMVP + fragsZeus + fragsSmile) / 5;
-        double pointsTeam2 = (fragsDendi + fragsXBOCT + fragsPuppey + fragsSolo + fragsDread) / 5;
+        double pointsTeam1 = (fragsBoom + fragsLittle + fragsMVP + fragsZeus + fragsSmile) / 5d;
+        double pointsTeam2 = (fragsDendi + fragsXBOCT + fragsPuppey + fragsSolo + fragsDread) / 5d;
 
+        System.out.println();
         if (pointsTeam1 > pointsTeam2) {
-            System.out.println("Перемогла команда" + nameTeam1 + ", яка набрала " + pointsTeam1 + "фрагів.");
+            System.out.println("Перемогла команда " + nameTeam1 + ", яка набрала " + pointsTeam1 + " фрагів.");
+        } else if (pointsTeam1 < pointsTeam2){
+            System.out.println("Перемогла команда " + nameTeam2 + ", яка набрала " + pointsTeam2 + " фрагів.");
         } else {
-            System.out.println("Перемогла команда" + nameTeam2 + ", яка набрала " + pointsTeam2 + "фрагів.");
+            System.out.println("Кількість фрагів рівна, переможця не визначено !!! Потрібен додатковий раунд.");
         }
-
-
-
-
-
-
-
 
 
     }
