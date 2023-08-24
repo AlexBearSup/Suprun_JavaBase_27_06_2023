bce709416649c9864740f604f8288a486457b944
Suprun Oleksandr <1specktr1@gmail.com>
2023-08-24 21:10:58 +0300

    add homeWork 18 Unit Testing

---
+++ src/main/java/ua/hillel/suprun/homeworks/homework18/ArrayUtil.java
@@ -0,0 +1,52 @@
+package ua.hillel.suprun.homeworks.homework18;
+
+public class ArrayUtil {
+
+//    null = -3
+//    empty = -2
+    // "считает среднее число" = 1
+
+
+
+    public static int averageNumber (int[] array){
+        if (array == null){
+            return -3;
+        }
+        if (array.length == 0 ){
+            return -2;
+        }
+        int x = 0;
+        int average;
+        for (int i = 0; i < array.length; i++) {
+            x += array[i];
+        }
+        average = x / array.length;
+//        System.out.println("Середньє значення массиву = " + average);
+        return 1;
+    }
+
+
+
+    //    null = -3
+    //    empty = -2
+    //    "зубчатый" = -1
+    //    kvadrat = 1
+
+    public static int squareArray (int[][] arrayD){
+        if (arrayD == null){
+            return -3;
+        }
+        int numberOfRows = arrayD.length;
+        if (numberOfRows == 0){
+            return -2;
+        }
+        int numberOfcolumns = arrayD[0].length;
+        for (int i = 0; i < arrayD.length; i++) {
+            if (arrayD[i].length != numberOfcolumns){
+                return -1;
+            }
+        }
+        return 1;
+    }
+
+}
--- src/test/java/ua/hillel/suprun/homeworks/homework18/TestArrayUtil.java
+++ src/test/java/ua/hillel/suprun/homeworks/homework18/TestArrayUtil.java
@@ -1,8 +1,65 @@
 package ua.hillel.suprun.homeworks.homework18;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class TestArrayUtil {
+    static int[] arr1;
+    static int[][] arr2;
+//
+//    @BeforeAll
+//    static void bfall(){
+//        arr1 = new int[] {1,2,3,4,5,6,7};
+//        arr2 = new int[][] {{1,2,3},{1,2,3},{1,2,3}};
+//    }
+
+    //  тестирование 1го метода, считает ли среднее значение
+
+    @Test
+    void test11(){
+        Assertions.assertEquals(-3, ArrayUtil.averageNumber(null));
+    }
+    @Test
+    void test12(){
+        arr1 = new int[] {};
+        Assertions.assertEquals(-2, ArrayUtil.averageNumber(arr1));
+    }
+    @Test
+    void test13() {
+        arr1 = new int[]{1, 2, 3, 4, 5, 6, 7};
+        Assertions.assertEquals(1, ArrayUtil.averageNumber(arr1));
+    }
+
+    //  тестирование 2го метода, КВАДРТАНЫЙ ЛИ 2мерный массив
+
+    @Test
+    void test21(){
+        Assertions.assertEquals(-3, ArrayUtil.squareArray(null));
+    }
+    @Test
+    void test22(){
+        arr2 = new int[][] {};
+        Assertions.assertEquals(-2, ArrayUtil.squareArray(arr2));
+    }
+    @Test
+    void test23(){
+        arr2 = new int[][] {{1,2,3},{1,2},{1,2,3,4}};
+        Assertions.assertEquals(-1, ArrayUtil.squareArray(arr2));
+    }
+    @Test
+    void test24(){
+        arr2 = new int[][] {{1,2,3},{1,2,3},{1,2,3}};
+        Assertions.assertEquals(1, ArrayUtil.squareArray(arr2));
+    }
+
+
+
+
+
+
+
+
+
 
 }
